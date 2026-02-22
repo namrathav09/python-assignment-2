8bc80410b22f81dd425cc7418a16209e28fc0c98
namrathav09 <namrathavenkateshc@gmail.com>
2026-02-22 09:26:13 +0530

    Added python assignment 2

---
+++ simplecalculator.py
@@ -0,0 +1,17 @@
+a=int(input("Enter a number A:"))
+b=int(input("Enter a number B:"))
+
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+modulus=a%b
+exponentiation=a**b
+
+print(f"Addition {a} and {b} gives:",addition)
+print(f"Subtraction of {a} and {b} gives:",subtraction)
+print(f"Mulplication of {a} and {b} gives:",multiplication)
+print(f"Division of {a} and  {b} gives:",division)
+print(f"Modulus of {a} and {b} gives:",modulus)
+print(f"Exponention of {a} ^ {b} gives:",exponentiation)
+
